8abc0dcb1b5b3650dff75f78905a885cbf4916b4
AlilsonWolf <techangel421@gmail.com>
2024-04-11 22:15:57 -0300

    building admin area

---
+++ EventHub/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace EventHub.Controllers
+{
+
+    public class AdminController : Controller
+    {
+        private readonly ILogger<AdminController> _logger;
+
+        public AdminController(ILogger<AdminController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
---
+++ EventHub/Controllers/EventoController.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EventHub.Data;
+using EventHub.Models;
+
+namespace EventHub.Controllers
+{
+    public class EventoController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public EventoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Evento
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Evento.ToListAsync());
+        }
+
+        // GET: Evento/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var evento = await _context.Evento
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (evento == null)
+            {
+                return NotFound();
+            }
+
+            return View(evento);
+        }
+
+        // GET: Evento/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Evento/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Titulo,Descricao,DataEvento,Localizacao,LinkVideo")] Evento evento)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(evento);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(evento);
+        }
+
+        // GET: Evento/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var evento = await _context.Evento.FindAsync(id);
+            if (evento == null)
+            {
+                return NotFound();
+            }
+            return View(evento);
+        }
+
+        // POST: Evento/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Titulo,Descricao,DataEvento,Localizacao,LinkVideo")] Evento evento)
+        {
+            if (id != evento.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(evento);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EventoExists(evento.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(evento);
+        }
+
+        // GET: Evento/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var evento = await _context.Evento
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (evento == null)
+            {
+                return NotFound();
+            }
+
+            return View(evento);
+        }
+
+        // POST: Evento/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var evento = await _context.Evento.FindAsync(id);
+            if (evento != null)
+            {
+                _context.Evento.Remove(evento);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EventoExists(int id)
+        {
+            return _context.Evento.Any(e => e.ID == id);
+        }
+    }
+}
